796e07c5167a9b65d181c73e94eff0c1649ee692
sunil8446 <chaudhary.sun@northeastern.edu>
2026-04-13 21:54:53 -0400

    Update site metadata and loader branding

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Sunil Chaudhary',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Sunil Chaudhary is a data and cloud-focused technologist specializing in data engineering, analytics, automation, and cloud workflows.',
+    siteUrl: 'https://sunil8446.github.io/v4', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@sunil8446',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Sunil Chaudhary',
+        short_name: 'Sunil Chaudhary',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
