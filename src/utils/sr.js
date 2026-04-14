e79739f068485976299de78ba415c1ffc81c2ec5
sunil8446 <chaudhary.sun@northeastern.edu>
2026-04-13 22:19:33 -0400

    Fix SSR issue for ScrollReveal

--- src/utils/sr.js
+++ src/utils/sr.js
@@ -1,6 +1,30 @@
-import ScrollReveal from 'scrollreveal';
+let sr = null;
 
-const isSSR = typeof window === 'undefined';
-const sr = isSSR ? null : ScrollReveal();
+const loadScrollReveal = async () => {
+  if (typeof window === 'undefined') {return null;}
+  if (sr) {return sr;}
 
-export default sr;
+  const module = await import('scrollreveal');
+  sr = module.default ? module.default() : module();
+  return sr;
+};
+
+const srSafe = {
+  reveal: async (...args) => {
+    const instance = await loadScrollReveal();
+    if (instance) {return instance.reveal(...args);}
+    return null;
+  },
+  clean: async (...args) => {
+    const instance = await loadScrollReveal();
+    if (instance && instance.clean) {return instance.clean(...args);}
+    return null;
+  },
+  destroy: async (...args) => {
+    const instance = await loadScrollReveal();
+    if (instance && instance.destroy) {return instance.destroy(...args);}
+    return null;
+  },
+};
+
+export default srSafe;
