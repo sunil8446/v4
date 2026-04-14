481be6e5e85078e28f1cd57b645a2a48b0f4e74c
sunil8446 <chaudhary.sun@northeastern.edu>
2026-04-13 21:24:44 -0400

    Clean up template references

--- src/components/footer.js
+++ src/components/footer.js
@@ -77,7 +77,7 @@ const Footer = () => {
     if (process.env.NODE_ENV !== 'production') {
       return;
     }
-    fetch('https://api.github.com/repos/bchiang7/v4')
+    fetch('https://api.github.com/repos/sunil8446/v4')
       .then(response => response.json())
       .then(json => {
         const { stargazers_count, forks_count } = json;
@@ -105,7 +105,7 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
+        <a href="https://github.com/sunil8446/v4">
           <div>Designed & Built by Sunil Chaudhary</div>
 
           {githubInfo.stars && githubInfo.forks && (
